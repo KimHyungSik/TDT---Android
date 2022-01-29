81a0ab9773a543f3fd24861ca31b478b70a76a92
hwanggyuil <hwanggyuil@gmail.com>
2022-01-29 16:58:09 +0900

    login api commit

--- app/src/main/java/com/todotracks/tdt/MainActivity.kt
+++ app/src/main/java/com/todotracks/tdt/MainActivity.kt
@@ -19,4 +19,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
+
 }
--- app/src/main/java/com/todotracks/tdt/src/user_settings/UserSettingActivity.kt
+++ app/src/main/java/com/todotracks/tdt/src/user_settings/UserSettingActivity.kt
@@ -2,6 +2,7 @@ package com.todotracks.tdt.src.user_settings
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.todotracks.tdt.MainActivity
 import com.todotracks.tdt.R
@@ -9,20 +10,42 @@ import com.todotracks.tdt.databinding.ActivityMainBinding
 import com.todotracks.tdt.databinding.ActivityUserSettingBinding
 import com.todotracks.tdt.kotlin.config.BaseActivity
 import com.todotracks.tdt.src.map.MapActivity
+import com.todotracks.tdt.src.user_settings.model.loginRequest
+import com.todotracks.tdt.src.user_settings.model.loginResponse
+import com.todotracks.tdt.src.user_settings.service.LoginService
+import com.todotracks.tdt.src.user_settings.service.LoginView
 
-class UserSettingActivity : BaseActivity<ActivityUserSettingBinding>(ActivityUserSettingBinding::inflate) {
+class UserSettingActivity : BaseActivity<ActivityUserSettingBinding>(ActivityUserSettingBinding::inflate), LoginView{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        var token_id =  Settings.Secure.getString(
+            applicationContext.contentResolver, Settings.Secure.ANDROID_ID
+        )
+//        showCustomToast(token_id)
+
         binding.settingOkBtn.setOnClickListener {
-            var intent = Intent(this, MapActivity::class.java)
-            startActivity(intent)
+            var loginReq = loginRequest(token_id.toString())
+            LoginService(this).tryPostLogin(loginReq)
+//            my_device_token(token_id)
         }
 
         binding.settingBtn.setOnClickListener {
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onPostLoginSuccess(response: loginResponse) {
+        var userId = response.member_no
+        my_user_Id(userId)
+        showCustomToast(userId.toString())
+        var intent = Intent(this, MapActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onPostLoginFailure(message: String) {
+        TODO("Not yet implemented")
     }
 }
