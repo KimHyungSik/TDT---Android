0538df89dc336425b18a7d2e9378516d80ce291a
KimHyungSik <colrog968@gmail.com>
2022-01-29 12:01:27 +0900

    first commit

---
+++ app/src/main/java/com/todotracks/tdt/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.todotracks.tdt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.todotracks.tdt.databinding.ActivityMainBinding
+import com.todotracks.tdt.ui.theme.TDTTheme
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.composeView.setContent {
+            Text(text = "text")
+        }
+    }
+}
